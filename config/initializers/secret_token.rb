5a84c99461395fb0310adc7369f878187ee15cd3
Candi Edie <candi@designgroupink.com>
2013-11-09 12:41:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Designgroupink::Application.config.secret_key_base = '3c9e63eeb686511d10157dbcc32c2876cee5df49bc94d12a8421e3befd211fde1343f366d6b418ad2b2aeb359a43c9a19671ccaee96de76a1cc49f0269e71361'
